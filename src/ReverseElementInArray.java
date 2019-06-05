be6833b7e3bf41068c7bfc8b081b53fbf7aa3c95
nvgiang2212 <giangnguyen221295@gmail.com>
2019-06-05 15:27:05 +0700

    add btap

---
+++ src/ReverseElementInArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ReverseElementInArray {
+    public static void main(String[] args) {
+        // Khai bao bien
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+
+        // Nhap va kiem tra kich thuoc mang
+        do {
+            System.out.println("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20) {
+                System.out.println("Size does not exceed 20");
+            }
+        } while (size > 20);
+
+        // Nhap gia tri cho cac phan tu mang
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.println("Enter element " + (i+1) + ": ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+
+        System.out.printf("%-20s%s", "Element in array: ", "");
+        for (int j = 0; j < array.length; j++) {
+            System.out.println(array[j] + "\t");
+        }
+        
+    }
+}
